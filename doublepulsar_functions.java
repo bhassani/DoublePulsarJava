3155aef73f46d2fcd5ab0b7f8dc31958f83d27cd
bhassani <bhassani@users.noreply.github.com>
2021-09-17 15:32:10 -0500

    add readFile function

--- doublepulsar_functions.java
+++ doublepulsar_functions.java
@@ -30,6 +30,25 @@ public class Encryptor {
 	}
 }
 
+/*
+https://javarevisited.blogspot.com/2020/04/7-examples-to-read-file-into-byte-array-in-java.html#axzz76kuulJgU
+*/
+//readFile into buffer
+public static byte[] readFile(String file) throws IOException {   
+  File f = new File(file);
+  byte[] buffer = new byte[(int)f.length()]; 
+  FileInputStream is = new FileInputStream(file);  
+  is.read(buffer);   
+  is.close();   
+  return buffer;
+}
+
+/* implemention 2:
+
+import java.io
+https://docs.oracle.com/javase/9/docs/api/java/io/FileInputStream.html
+*/
+
 
 
 
